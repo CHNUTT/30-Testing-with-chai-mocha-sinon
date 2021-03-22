6aa9ee7709f9099485270dd39df9e0bf980685f4
CHNUTT <65862558+CHNUTT@users.noreply.github.com>
2021-03-21 19:15:28 -0500

    [ Test ] - 2. using sinon stub to mock the function

--- server/test/auth-middleware.test.js
+++ server/test/auth-middleware.test.js
@@ -1,13 +1,50 @@
 const { expect } = require('chai');
+const jwt = require('jsonwebtoken');
+const sinon = require('sinon');
 
 const authMiddleware = require('../src/middlewares/is-auth');
 
-it('should throw an error if no authorization header passing in', () => {
-  const req = {
-    get: function (headerName) {
-      return null;
-    },
-  };
+describe('isAuth middleware', () => {
+  it('should throw an error if no authorization header passing in', () => {
+    const req = {
+      get: function (headerName) {
+        return null;
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+      'Not Authorized'
+    );
+  });
 
-  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw('Not Authorized');
+  it('should throw an error if the Authrorization header is only one string', () => {
+    const req = {
+      get: function (headerName) {
+        return 'token';
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it('should throw an error if token cannot be verify', () => {
+    const req = {
+      get: function (headerName) {
+        return 'Bearer token';
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it('should yield a userId after decoding token', () => {
+    const req = {
+      get: function (headerName) {
+        return 'Bearer token';
+      },
+    };
+    sinon.stub(jwt, 'verify');
+    jwt.verify.returns({ userId: 'id' });
+    authMiddleware(req, {}, () => {});
+    expect(req).to.have.property('userId');
+    expect(req).to.have.property('userId', 'id');
+    jwt.verify.restore();
+  });
 });
